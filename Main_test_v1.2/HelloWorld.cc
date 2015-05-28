8b5e4aa373e426892639906903aa75171e97fe1a
andresfgarcia150 <andrespipe150@gmail.com>
2015-05-28 10:23:39 -0500

    Second version of simple compilable c++ file with Root, Delphes and HepMC

---
+++ Main_test_v1.2/HelloWorld.cc
@@ -0,0 +1,23 @@
+/*
+-------     Universidad de los Andes      -------
+-------      Departamento de Física       -------
+-------   Proyecto Joven Investigador     -------
+-------  Andrés Felipe García Albarracín  -------
+-------    Juan Carlos Sanabria Arenas    -------
+
+Hello world program. Write your c++ program using
+Root and/or HepMC libraries
+
+*/
+
+#include <iostream>
+// Comment the following lines if you do not need ROOT or HepMC
+#include "ROOTFunctions.h"
+//#include "HepMCFunctions.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hola mundo" << endl;
+	return 0;
+}
